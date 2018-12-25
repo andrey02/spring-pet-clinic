06acef4fc103b3901b844bd88ca0d3b63bd04386
andrey02 <andreiteixeira02@gmail.com>
2018-12-25 11:10:22 -0200

    Implementing index controller and html page. closes #7

---
+++ pet-clinic-web/src/main/java/com/andrei/springpetclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.andrei.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
+public class IndexController {
+
+    @RequestMapping({"index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
