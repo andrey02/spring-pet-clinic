e5b0c25e14294af6d4408f206a44753f194fadb1
andrey02 <andreiteixeira02@gmail.com>
2018-12-29 11:23:47 -0200

    new thing added

---
+++ pet-clinic-data/src/main/java/com/andrei/springpetclinic/Model/Owner.java
@@ -0,0 +1,8 @@
+package com.andrei.springpetclinic.model;
+
+public class Owner extends Person {
+
+    public Owner(Long id, String firstName, String lastName) {
+        super(id, firstName, lastName);
+    }
+}
